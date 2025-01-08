cb91d35c20245908e60872ce7d4a701ac1ca451a
AXurshidbek <abdubannobovx@gmail.com>
2025-01-08 19:03:59 +0500

    second

--- LevelUp/settings.py
+++ LevelUp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r4jk8!)mh&)e+=vt$f6u8ukf89c4ivj@+x3fxroi_6&!4hwq0y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -117,7 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS= [BASE_DIR/'static']
+if DEBUG:
+    STATICFILES_DIRS= [BASE_DIR/'static']
+else:
+    STATIC_ROOT = BASE_DIR/'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR/'media/'
